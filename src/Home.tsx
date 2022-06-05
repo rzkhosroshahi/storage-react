a4ecf8176d596238f2463a1c7fb148ae91f55362
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2022-06-05 19:25:41 +0430

    use custom event to change routes from host

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
 import { Outlet, Link } from "react-router-dom";
 
 export default () => {
+    let navigate = useNavigate();
+    const handleNavigateFromHost = (e) => {
+        navigate(e.detail.path);
+    };
+    useEffect(() => {
+        addEventListener('changeRoute', handleNavigateFromHost);
+
+        return function() {
+            removeEventListener('changeRoute', handleNavigateFromHost);
+        };
+    }, []);
+    addEventListener('changeRoute', (e) => console.log('e>>', e));
+
     return (
         <div>
             <h1>Storage ReactJs App</h1>
